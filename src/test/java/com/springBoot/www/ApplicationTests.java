eaf0e1a91d64c631c75e03327ebb825ffc257810
david95j2 <glory20220j@gmail.com>
2021-07-31 15:37:14 +0900

    junit4 가 아닌 junit5를 사용

--- src/test/java/com/springBoot/www/ApplicationTests.java
+++ src/test/java/com/springBoot/www/ApplicationTests.java
@@ -1,18 +1,28 @@
 package com.springBoot.www;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DisplayName("특수 테스트\uD83D\\uDE00")
 class ApplicationTests {
-
+	
+	private int sum;
+	
 	@Test
+	@DisplayName("첫번째 테스트")
 	void contextLoads() {
 		//given
-
+		sum = 1;
+		
 		//when
+		assertFalse(true, "Exception!!");
 
 		//then
+
 	}
 
 }
