c2949642e3862882acd94e7f583730bd92f468cc
david95j2 <glory20220j@gmail.com>
2021-08-02 23:36:35 +0900

    gitignore 수정

--- src/main/java/com/springBoot/www/dto/MemberDto.java
+++ src/main/java/com/springBoot/www/dto/MemberDto.java
@@ -1,11 +1,6 @@
 package com.springBoot.www.dto;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public class MemberDto {
-	private final String loginId;
-	private final String loginPw;
+	private  String loginId;
+	private  String loginPw;
 }
--- src/test/java/com/springBoot/www/ApplicationTests.java
+++ src/test/java/com/springBoot/www/ApplicationTests.java
@@ -1,28 +1,13 @@
 package com.springBoot.www;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@DisplayName("특수 테스트\uD83D\\uDE00")
-class ApplicationTests {
-	
-	private int sum;
-	
-	@Test
-	@DisplayName("첫번째 테스트")
-	void contextLoads() {
-		//given
-		sum = 1;
-		
-		//when
-		assertFalse(true, "Exception!!");
+public class ApplicationTests {
 
-		//then
-
-	}
-
+    @Test
+    public void ContextLoad() {
+        System.out.println("머냐");
+    }
 }
