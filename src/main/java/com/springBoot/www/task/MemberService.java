52545782f052d654a22b721c9d375b7f21e9ce53
david95j2 <glory20220j@gmail.com>
2021-08-10 23:17:20 +0900

    .gradle gitignore에서 제거

--- src/main/java/com/springBoot/www/task/MemberService.java
+++ src/main/java/com/springBoot/www/task/MemberService.java
@@ -1,7 +1,14 @@
 package com.springBoot.www.task;
 
+import com.springBoot.www.task.request.serach.MemberSerachRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
+    public String showUser(MemberSerachRequest request) {
+        return "LoginId : " + request.getLoginId() + " \n"
+                + "LoginPw : " + request.getLoginPw() + " \n"
+                + "Id : " + request.getId() + " \n"
+                + "Email : " + request.getEmail() + " \n";
+    }
 }
